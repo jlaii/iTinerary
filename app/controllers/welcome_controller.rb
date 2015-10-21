9867b1208403d05138e6bd135ae0c1952dea3462
Eleane Jao <eleanew83@berkeley.edu>
2015-10-21 16:24:43 -0700

    DateTime correctly stored.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,14 @@
 class WelcomeController < ApplicationController
 	def save
 		name = params[:name]
-		#TODO: transform start/end date to DateTime
-	    start_time = params[:startdate]
-	    end_time = params[:enddate]
+	    start_lst = params[:startdate].split("/")
+	    end_lst = params[:enddate].split("/")
+	    start_time = DateTime.new(start_lst[2].to_i, start_lst[0].to_i, start_lst[1].to_i)
+	    end_time = DateTime.new(end_lst[2].to_i, end_lst[0].to_i, end_lst[1].to_i)
 
 	    newTrip = Trip.new(name: name, start_time: start_time, end_time: end_time)
 	    newTrip.save
-	    
+
 		redirect_to attractions_path
 	end
 end
