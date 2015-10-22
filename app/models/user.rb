38f1338ee4383affa144278ff056900f5c1d9f3a
Joseph Chiang <josephchiang28@gmail.com>
2015-10-22 00:16:54 -0700

    Can successfully login with Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,11 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.email = auth.info.email
-      user.encrypted_password = auth.credentials.token
+      # user.encrypted_password = auth.credentials.token
+      user.password = Devise.friendly_token[0,20]
       logger.debug "--------------------Printing email--------------------"
       # logger.debug auth.info.name
       logger.debug user.first_name
