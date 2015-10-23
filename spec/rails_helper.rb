f38c5e4834f03d12fe7fef038c80f25e4672e4f8
Justin Lai <justin.lai12@gmail.com>
2015-10-23 16:15:51 -0700

    fix welcome_controller_spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -50,4 +51,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+end
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
 end
