d58dd72dc5746b035e2d45b5563f58bcf30cd276
Eleane Jao <eleanew83@berkeley.edu>
2015-10-23 00:08:20 -0700

    UnitTests for welcome controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,14 @@
 class WelcomeController < ApplicationController
 	def save
-    params[:destination] = params[:destination].titleize
-		name = params[:destination]
-    start_lst = params[:startdate].split("/")
-    end_lst = params[:enddate].split("/")
-    start_time = DateTime.new(start_lst[2].to_i, start_lst[0].to_i, start_lst[1].to_i)
-    end_time = DateTime.new(end_lst[2].to_i, end_lst[0].to_i, end_lst[1].to_i)
+    	params[:destination] = params[:destination].titleize
+		city = params[:destination]
+    	start_lst = params[:startdate].split("/")
+    	end_lst = params[:enddate].split("/")
+    	start_time = DateTime.new(start_lst[2].to_i, start_lst[0].to_i, start_lst[1].to_i)
+    	end_time = DateTime.new(end_lst[2].to_i, end_lst[0].to_i, end_lst[1].to_i)
 
-    newTrip = Trip.new(name: name, start_time: start_time, end_time: end_time)
-    newTrip.save
+    	newTrip = Trip.new(city: city, start_time: start_time, end_time: end_time)
+    	newTrip.save
 
 		redirect_to show_attractions_path(params)
 	end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
 class Trip < ActiveRecord::Base
   has_many :trip_attractions
   has_and_belongs_to_many :user
+  validate :trip_date_validation
+
+  def trip_date_validation
+  	if self.start_time.to_i > self.end_time.to_i
+  		errors[:base] << "start date must be prior to end date"
+  	end
+  end
 end
