1c08eafa08360fa9f3700543fd435f1e206765e1
Joseph Chiang <josephchiang28@gmail.com>
2015-10-22 23:13:29 -0700

    Removed test root route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'welcome#welcome_login'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
