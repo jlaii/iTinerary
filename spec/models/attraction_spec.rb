793e3a65c2423eae8b370f4c0747c4acea3b7920
Justin Lai <justin.lai12@gmail.com>
2015-11-04 15:42:32 -0800

    add test for attractions that are always open

--- spec/models/attraction_spec.rb
+++ spec/models/attraction_spec.rb
@@ -50,11 +50,17 @@ RSpec.describe Attraction, type: :model do
 
   end
 
-  def sample_hours_json
-    JSON.parse(%q({"meta":{"code":200,"requestId":"563852f4498eef7582dde68c"},"notifications":[{"type":"notificationTray","item":{"unreadCount":0}}],"response":{"hours":{"timeframes":[{"days":[1,2,3,4,5],"includesToday":true,"open":[{"start":"0800","end":"1600"},{"start":"1800","end":"2330"}],"segments":[]},{"days":[6],"open":[{"start":"0900","end":"1600"},{"start":"1800","end":"2330"}],"segments":[]},{"days":[7],"open":[{"start":"0900","end":"1800"}],"segments":[]}]},"popular":{"timeframes":[{"days":[2],"includesToday":true,"open":[{"start":"0800","end":"1400"},{"start":"1800","end":"2100"}],"segments":[]},{"days":[3],"open":[{"start":"0800","end":"1500"},{"start":"1900","end":"2100"}],"segments":[]},{"days":[4],"open":[{"start":"0800","end":"1500"},{"start":"1800","end":"2100"}],"segments":[]},{"days":[5],"open":[{"start":"0800","end":"1500"},{"start":"1800","end":"2200"}],"segments":[]},{"days":[6],"open":[{"start":"0800","end":"1600"},{"start":"1800","end":"2200"}],"segments":[]},{"days":[7],"open":[{"start":"0800","end":"1700"}],"segments":[]},{"days":[1],"open":[{"start":"0800","end":"1500"},{"start":"1800","end":"2100"}],"segments":[]}]}}}))["response"]
-  end
 
   context "check attraction's hours" do
+    def sample_hours_json
+      JSON.parse(%q({"meta":{"code":200,"requestId":"563852f4498eef7582dde68c"},"notifications":[{"type":"notificationTray","item":{"unreadCount":0}}],"response":{"hours":{"timeframes":[{"days":[1,2,3,4,5],"includesToday":true,"open":[{"start":"0800","end":"1600"},{"start":"1800","end":"2330"}],"segments":[]},{"days":[6],"open":[{"start":"0900","end":"1600"},{"start":"1800","end":"2330"}],"segments":[]},{"days":[7],"open":[{"start":"0900","end":"1800"}],"segments":[]}]},"popular":{"timeframes":[{"days":[2],"includesToday":true,"open":[{"start":"0800","end":"1400"},{"start":"1800","end":"2100"}],"segments":[]},{"days":[3],"open":[{"start":"0800","end":"1500"},{"start":"1900","end":"2100"}],"segments":[]},{"days":[4],"open":[{"start":"0800","end":"1500"},{"start":"1800","end":"2100"}],"segments":[]},{"days":[5],"open":[{"start":"0800","end":"1500"},{"start":"1800","end":"2200"}],"segments":[]},{"days":[6],"open":[{"start":"0800","end":"1600"},{"start":"1800","end":"2200"}],"segments":[]},{"days":[7],"open":[{"start":"0800","end":"1700"}],"segments":[]},{"days":[1],"open":[{"start":"0800","end":"1500"},{"start":"1800","end":"2100"}],"segments":[]}]}}}))["response"]
+    end
+
+    def always_open_sample_hours_json
+      JSON.parse(%q({"meta":{"code":200,"requestId":"563a96d2498eac20c1905d85"},"response":{"hours":{"timeframes":[{"days":[1,2,3,4,5,6,7],"includesToday":true,"open":[{"start":"0000","end":"+0000"}],"segments":[]}]},"popular":{"timeframes":[{"days":[3],"includesToday":true,"open":[{"start":"1200","end":"2000"}],"segments":[]},{"days":[4],"open":[{"start":"1200","end":"1400"},{"start":"1800","end":"2000"}],"segments":[]},{"days":[5],"open":[{"start":"1200","end":"1400"},{"start":"1600","end":"2100"}],"segments":[]},{"days":[6],"open":[{"start":"1000","end":"1900"}],"segments":[]},{"days":[7],"open":[{"start":"1000","end":"2000"}],"segments":[]},{"days":[1],"open":[{"start":"1200","end":"2000"}],"segments":[]},{"days":[2],"open":[{"start":"1200","end":"2100"}],"segments":[]}]}}}))["response"]
+
+    end
+
     it "returns true if attraction is open" do
       fake_attraction = Attraction.new(:hours_json => sample_hours_json)
       expect(fake_attraction.is_open? 1, 800, 1600).to eq true
@@ -74,6 +80,17 @@ RSpec.describe Attraction, type: :model do
       expect(fake_attraction.is_open? 5, 800, 1601).to eq false
       expect(fake_attraction.is_open? 6, 900, 1601).to eq false
       expect(fake_attraction.is_open? 7, 900, 1801).to eq false
+    end
+
+    it "returns true if attraction is open 24/7" do
+      open_attraction = Attraction.new(:hours_json => always_open_sample_hours_json)
+      expect(open_attraction.is_open? 1, 0000, 2400).to eq true
+      expect(open_attraction.is_open? 2, 0000, 2400).to eq true
+      expect(open_attraction.is_open? 3, 0000, 2400).to eq true
+      expect(open_attraction.is_open? 4, 0000, 2400).to eq true
+      expect(open_attraction.is_open? 5, 0000, 2400).to eq true
+      expect(open_attraction.is_open? 6, 0000, 2400).to eq true
+      expect(open_attraction.is_open? 7, 0000, 2400).to eq true
     end
 
   end
