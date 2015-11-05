6727d564b49a193d1d8e59d06aae00a35b6da8d2
Justin Lai <justin.lai12@gmail.com>
2015-11-04 23:39:08 -0800

    use TripAttraction instead of Attraction objects

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -19,43 +19,42 @@ class Trip < ActiveRecord::Base
   def generate_itinerary
     num_days = 0
     start_time = 800
-    attractions = create_attractions(params)
+    #not sure if this is right way to convert to array of trip_attraction objects
+    trip_attractions = TripAttraction.where(:trip_id => self.id).to_a
     city = City.find_by_name(params[:city])
     curr_attraction = Attraction.new(:latitude => city.lat, :longitude => city.lng)
-    vote_hash = create_vote_hash()
     for i in 0...NUM_ATTRACTIONS * (self.end_time - self.start_time).to_i
-      trip_attraction_hash = self.get_next_trip_attraction(curr_attraction, attractions, start_time, vote_hash, num_days)
-      attractions.delete(trip_attraction_hash[:trip_attraction].attraction_id)
+      trip_attraction_hash = self.get_next_trip_attraction(curr_attraction, trip_attractions, start_time, num_days)
+      trip_attractions.delete(trip_attraction_hash[:trip_attraction])
       curr_attraction = next_attraction
       start_time += 200 + trip_attraction_hash[:travel_time]
       if i % NUM_ATTRACTIONS == 0
         num_days += 1
+        start_time = 800
       end
     end
-    return itinerary
   end
 
-  def get_next_trip_attraction(prev_attraction, attractions, start_time,  vote_hash, num_days)
+  def get_next_trip_attraction(prev_attraction, trip_attractions, start_time, num_days)
     best_score = -1
     best_attraction = nil
     date_time = self.start_time + num_days
     day_of_week = date_time.cwday
-    for attraction in attractions
+    for trip_attraction in trip_attractions
+      attraction = Attraction.find(trip_attraction.attraction_id)
       travel_time = Trip.calculate_travel_time_manhattan(prev_attraction, attraction, DRIVING_SPEED)
-      score = vote_hash[attraction] * VOTE_WEIGHT + travel_time * TRAVEL_WEIGHT + attraction.rating * FOURSQUARE_WEIGHT
+      score = trip_attraction.vote_count * VOTE_WEIGHT + travel_time * TRAVEL_WEIGHT + attraction.rating * FOURSQUARE_WEIGHT
       if score > best_score and attraction.is_open?(day_of_week, start_time, start_time + 200)
         best_score = score
-        best_attraction = attraction
+        best_attraction = trip_attraction
       end
     end
-    trip_attraction = TripAttraction.new(:start_time => DateTime.new(date_time.year, date_time.month,
-                                                                     date_time.day, start_time / 100),
-                                         :end_time => DateTime.new(date_time.year, date_time.month,
-                                                                   date_time.day, (start_time + 200) / 100),
-                                         :attraction_id => best_attraction.id,
-                                         :vote_count => vote_hash[best_attraction.id], :trip_id => self.id)
-    trip_attraction.save
-    {:trip_attraction => trip_attraction, :travel_time => travel_time}
+    best_attraction.update_attributes(:start_time => DateTime.new(date_time.year, date_time.month,
+                                                                  date_time.day, start_time / 100),
+                                      :end_time => DateTime.new(date_time.year, date_time.month,
+                                                                date_time.day, (start_time + 200) / 100))
+    best_attraction.save
+    {:trip_attraction => best_attraction, :travel_time => travel_time}
 
 
 
