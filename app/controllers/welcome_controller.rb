8be616e86a24074467bba15b49998f139b6de850
Justin Lai <justin.lai12@gmail.com>
2015-11-06 17:46:34 -0800

    fix heroku attractions page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
 	def save
-    redirect_to show_attractions_path(destination: params[:destination], startdate: params[:startdate], enddate: params[:enddate])
+    redirect_to show_attractions_path(destination: params[:destination].titleize, startdate: params[:startdate], enddate: params[:enddate])
 	end
 end
