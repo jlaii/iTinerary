41f36cc5c5571c846259ec16a236a2de91e6e95c
Justin Lai <justin.lai12@gmail.com>
2015-11-24 23:12:36 -0600

    mock api calls for feature test

--- spec/features/add_trip_to_user_spec.rb
+++ spec/features/add_trip_to_user_spec.rb
@@ -1,8 +1,15 @@
 require "rails_helper"
-
-
+require "Foursquare"
 
 RSpec.feature "Add Trip to User", :type => :feature do
+  def stub_api_calls
+      Foursquare.should_receive(:import_attractions).and_return(@fake_api_response)
+      Foursquare.should_receive(:import_hours).at_most(50).times.and_return(@fake_hours_response)
+  end
+  before(:all) do
+    @fake_api_response = File.read('spec/data/fake_api.json')
+    @fake_hours_response = File.read('spec/data/sample_hours.json')
+  end
 
   scenario "User signs up" do
     visit "/users/sign_up"
@@ -23,6 +30,7 @@ RSpec.feature "Add Trip to User", :type => :feature do
 
   scenario "User upvotes attractions" do
     skip "logged in user generates voted itinerary redirects back to root" do
+      stub_api_calls
       visit "/users/sign_in"
       fill_in "Email", :with => "test@email.com"
       fill_in "Password", :with => "12345678"
@@ -50,6 +58,7 @@ RSpec.feature "Add Trip to User", :type => :feature do
   end
 
   scenario "User upvotes no attractions" do
+      stub_api_calls
       visit "/users/sign_in"
       fill_in "Email", :with => "test@email.com"
       fill_in "Password", :with => "12345678"
@@ -57,23 +66,24 @@ RSpec.feature "Add Trip to User", :type => :feature do
       expect(page).to have_text("Signed in successfully.")
 
       visit "/"
-      fill_in "destination", :with => "New York"
+      fill_in "destination", :with => "San Francisco"
       fill_in "startdate", :with => "10/23/2015"
       fill_in "enddate", :with => "10/23/2015"
       click_button "Submit"
-      expect(page).to have_text("Attractions around New York")
+      expect(page).to have_text("Attractions around San Francisco")
 
       click_button "Let's go!", :match => :first
 
-      expect(page).to have_text("You are going to: New York")
+      expect(page).to have_text("You are going to: San Francisco")
       expect(page).to have_text("You haven't upvoted any attractions for this trip.")
 
       click_button "Generate Itinerary"
-      expect(page).to have_text("Your Itinerary for New York")
+      expect(page).to have_text("Your Itinerary for San Francisco")
   end
 
   scenario "Anonymous user upvotes attractions and generates itinerary" do
     skip "anonymous user generates voted itinerary redirects back to root" do
+      stub_api_calls
       visit "/"
       fill_in "destination", :with => "San Francisco"
       fill_in "startdate", :with => "10/23/2015"
@@ -95,19 +105,19 @@ RSpec.feature "Add Trip to User", :type => :feature do
   end
 
   scenario "Anonymous user upvotes zero attractions and generates itinerary" do
-      visit "/"
-      fill_in "destination", :with => "San Francisco"
-      fill_in "startdate", :with => "10/23/2015"
-      fill_in "enddate", :with => "10/23/2015"
-      click_button "Submit"
-      expect(page).to have_text("Attractions around San Francisco")
+    visit "/"
+    fill_in "destination", :with => "San Francisco"
+    fill_in "startdate", :with => "10/23/2015"
+    fill_in "enddate", :with => "10/23/2015"
+    click_button "Submit"
+    expect(page).to have_text("Attractions around San Francisco")
 
-      click_button "Let's go!", :match => :first
+    click_button "Let's go!", :match => :first
 
-      expect(page).to have_text("You are going to: San Francisco")
-      expect(page).to have_text("You haven't upvoted any attractions for this trip.")
+    expect(page).to have_text("You are going to: San Francisco")
+    expect(page).to have_text("You haven't upvoted any attractions for this trip.")
 
-      click_button "Generate Itinerary"
-      expect(page).to have_text("Your Itinerary for San Francisco")
+    click_button "Generate Itinerary"
+    expect(page).to have_text("Your Itinerary for San Francisco")
   end
 end
