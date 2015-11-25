f7a5baac35250e88bb3be7baae755a121a4ac0c6
Mandy Huang <mandyxhuang@gmail.com>
2015-11-25 14:54:42 -0800

    votes spec

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -65,11 +65,6 @@ RSpec.describe TripsController, type: :controller do
       expect(TripAttraction.count).to eq 3
     end
 
-    # it "change vote counts for trip_attraction id:1" do
-    #   post :new, :destination => "San Francisco", "1"=>"1", "2"=>"0", "3"=>"0"
-    #   expect(TripAttraction.find(1).vote_count).to eq 1
-    # end
-
     it "should fail to create trip- end_date before start_date" do
       post :new, :destination => "San Francisco", :startdate => "11/18/2015", :enddate => "11/16/2015", "1"=>"0", "2"=>"0", "3"=>"0"
       expect { raise "start_date later than end_date" }.to raise_error
