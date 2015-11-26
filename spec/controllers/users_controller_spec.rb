6f4d07af188559a72218b1cfeb9577a6a3a8772f
Joseph Chiang <josephchiang28@gmail.com>
2015-11-25 20:31:36 -0800

    Added unit test for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
+  before(:context) do
+    User.delete_all
+    Trip.delete_all
+    UserTrip.delete_all
+  end
 
+  # user = User.create(id: 1, email: "test@gmail.com")
+  login_user # suppose to create user with id=1
+  context "Test show" do
+    # byebug
+    it "checks if get the right amount of trips of a user" do
+      Trip.create(id: 1)
+      Trip.create(id: 2)
+      Trip.create(id: 3)
+      UserTrip.create(user_id: subject.current_user.id, trip_id: 1)
+      UserTrip.create(user_id: subject.current_user.id, trip_id: 2)
+      UserTrip.create(user_id: subject.current_user.id, trip_id: 3)
+      # byebug
+      post :show
+      expect(assigns(:trips).length).to equal(3)
+    end
+  end
 end
