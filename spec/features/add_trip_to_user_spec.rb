10bf2c085d15971a4b6cdb7a9dac7546ddf3d9c5
Justin Lai <justin.lai12@gmail.com>
2015-12-01 12:48:00 -0800

    remove skipped spec

--- spec/features/add_trip_to_user_spec.rb
+++ spec/features/add_trip_to_user_spec.rb
@@ -29,6 +29,7 @@ RSpec.feature "Add Trip to User", :type => :feature do
   end
 
   scenario "User upvotes no attractions" do
+    stub_api_calls
     visit "/users/sign_in"
     fill_in "Email", :with => "test@email.com"
     fill_in "Password", :with => "12345678"
@@ -36,35 +37,33 @@ RSpec.feature "Add Trip to User", :type => :feature do
     expect(page).to have_text("Signed in successfully.")
 
     visit "/"
-    fill_in "destination", :with => "New York"
+    fill_in "destination", :with => "San Francisco"
     fill_in "startdate", :with => "10/23/2015"
     fill_in "enddate", :with => "10/23/2015"
     click_button "Submit"
-    expect(page).to have_text("Attractions around New York")
+    expect(page).to have_text("Attractions around San Francisco")
 
     click_button "Let's go!", :match => :first
-    expect(page).to have_text("Your Itinerary for New York")
+    expect(page).to have_text("Your Itinerary for San Francisco")
   end
 
-  scenario "User upvotes attractions" do
-    # skipping this test because it passes only sometimes for some reason
-    skip "logged in user generates voted itinerary redirects back to root" do
-      visit "/users/sign_in"
-      fill_in "Email", :with => "test@email.com"
-      fill_in "Password", :with => "12345678"
-      click_button "Log in"
-      expect(page).to have_text("Signed in successfully.")
-
-      visit "/"
-      fill_in "destination", :with => "San Francisco"
-      fill_in "startdate", :with => "10/23/2015"
-      fill_in "enddate", :with => "10/23/2015"
-      click_button "Submit"
-      expect(page).to have_text("Attractions around San Francisco")
-
-      choose "1 Louise M. Davies Symphony Hall"
-      click_button "Let's go!", :match => :first
-      expect(page).to have_text("Your Itinerary for San Francisco")
-    end
-  end
+  # scenario "User upvotes attractions" do
+  #   Foursquare.should_receive(:import_attractions).and_return(@fake_api_response)
+  #   Foursquare.should_receive(:import_hours).at_most(50).times.and_return(@fake_hours_response)
+  #   visit "/users/sign_in"
+  #   fill_in "Email", :with => "test@email.com"
+  #   fill_in "Password", :with => "12345678"
+  #   click_button "Log in"
+  #   expect(page).to have_text("Signed in successfully.")
+  #
+  #   visit "/"
+  #   fill_in "destination", :with => "San Francisco"
+  #   fill_in "startdate", :with => "10/23/2015"
+  #   fill_in "enddate", :with => "10/23/2015"
+  #   click_button "Submit"
+  #   expect(page).to have_text("Attractions around San Francisco")
+  #
+  #   click_button "Let's go!", :match => :first
+  #   expect(page).to have_text("Your Itinerary for San Francisco")
+  # end
 end
